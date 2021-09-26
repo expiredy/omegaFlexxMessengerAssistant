eb2783f86743cbbf562fed4d1af6f6ee7b9f56d5
Expiredy101 <yaraoley2005@mail.ru>
2021-09-26 15:09:04 +0300

    Some ai logging and connecting

--- MessagePreprocessorManager.py
+++ MessagePreprocessorManager.py
@@ -6,12 +6,4 @@ LOWER_KARMA_LIMITER = -5
 
 def message_preprocessor(message_text: str, chat_messages_data_dict: dict):
 
-    def get_chat_history_karma(chat_information_dict: dict) -> float:
-        for id_of_message in (chat_information_dict.keys()):
-            pass
-        return 0.0
-
-    def get_message_analyzed_by_ai_info(text_for_processing: str, user_karma_count: int) -> dict:
-        pass
-
-    previews_karma_counter = get_chat_history_karma(chat_messages_data_dict)
+    previews_karma_counter = MessageAiProcessing.get_messege_processing_info(message_text, chat_messages_data_dict)
